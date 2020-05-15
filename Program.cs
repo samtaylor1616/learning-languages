0ba117cd7c0f9b6dc5b9661489019989a7ba09d2
Sam Taylor <sammi1616@live.com>
2020-05-15 10:18:24 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TeleprompterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = ReadFrom("sampleQuotes.txt");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        static IEnumerable<string> ReadFrom(string filename)
+        {
+            string line;
+            using (var reader = File.OpenText(filename))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
